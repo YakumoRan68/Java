0abe935b2230d27812f29caf9c6332fb7c22aa62
YakumoRan68 <yys14239103@gmail.com>
2019-11-07 13:36:25 +0900

    1101

--- 1101/Box.java
+++ 1101/Box.java
@@ -1,18 +1,28 @@
 public class Box {
 	private int width, height;
-	private char fillChar;
+	private char fillChar = ' ';
 	
 	public Box() {
-		draw(10, 1);
+		this.width = 10;
+		this.height = 1;
 	}
 	
-	public void draw(int width, int height) {
+	public Box(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void draw() {
 		String str = "";
 		for (int i = 0; i < height; i++) {
 			for(int j = 0; j < width; j++) 
-				str += "*";
+				str += fillChar;
 			str += "\n";
 		}
 		System.out.print(str);
+	}
+	
+	public void fill(char c) {
+		fillChar = c;
 	}
 }
--- 1101/ch4_p6.java
+++ 1101/ch4_p6.java
@@ -1,4 +1,10 @@
-
 public class ch4_p6 {
-
+	public static void main(String[] args) {
+		Box a = new Box();
+		Box b = new Box(20, 3);
+		a.fill('*');
+		b.fill('%');
+		a.draw();
+		b.draw();
+	}
 }
