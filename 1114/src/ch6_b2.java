1469c425d4394ed012fe47acd6fcc1254f5dc97c
YakumoRan <yys14239103@gmail.com>
2019-11-28 13:54:23 +0900

    1128

--- 1114/src/ch6_b2.java
+++ 1114/src/ch6_b2.java
@@ -6,19 +6,23 @@ public class ch6_b2 {
 		while(true) {
 			System.out.print(">>");
 			String s = scanner.nextLine();
-			StringBuffer sb = new StringBuffer();
-			if(sb.equals("exit")) {
+			StringBuffer sb = new StringBuffer(s);
+			if(sb.equals(new StringBuffer("exit"))) {
 				System.out.println("종료합니다...");
 				break;
-			}
-			int index = (int)(Math.random() * s.length());
-			while(true) {
-				int i = (int)(Math.random()*26);
-				char c = (char)('a' + i);
-				if(sb.charAt(index) != c) {
-					//sb.replace(index, index + 1, c);
+			} else {
+				int index = (int)(Math.random() * s.length());
+				while(true) {
+					int i = (int)(Math.random()*26);
+					char c = (char)('a' + i);
+					if(sb.charAt(index) != c) {
+						sb.replace(index, index + 1, c + "");
+						break;
+					}
 				}
+				System.out.println(sb);
 			}
 		}
+		scanner.close();
 	}
 }
